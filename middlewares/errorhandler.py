9dceb22316f49a91332f2775243f95030821ad73
Muhammad Talha <mt109208@gmail.com>
2026-04-01 14:40:03 +0500

    error handled

--- middlewares/errorhandler.py
+++ middlewares/errorhandler.py
@@ -2,5 +2,5 @@ from fastapi import HTTPException,Request
 from starlette.responses import JSONResponse
 def httpexception(req:Request,exc:HTTPException):
     return JSONResponse(status_code=exc.status_code,content={"error":exc.detail})
-def exceptions(req:Request,exc:Exceptions):
+def exceptions(req:Request,exc:Exception):
     return JSONResponse(status_code=500,content={"error":str(exc)})
